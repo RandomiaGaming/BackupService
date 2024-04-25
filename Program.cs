f6ba435ab3eeb60f72ba3fa01a73b99f06e573d1
Finlay Christ <RandomiaGaming@gmail.com>
2024-04-24 22:55:33 -0700

    Backup Commit

--- Program.cs
+++ Program.cs
@@ -1,152 +1,278 @@
-﻿namespace BackupService
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BackupService
 {
     public static class Program
     {
-        public static long BackedUpFileCount = 0;
+        public static string SourceDrivePath;
+        public static string DestinationDrivePath;
         public static void Main()
         {
-            Backup();
-        }
-        public static void Backup()
-        {
-            try
+            Console.WriteLine("It\'s Backup Time Motha Fuckers...");
+            Console.WriteLine();
+            Console.WriteLine("PLEASE DO NOT USE SOURCE OR DESTINATION DRIVE DURING BACKUP!!!");
+            Console.WriteLine();
+            if (IdentifyDrives())
             {
-                Console.WriteLine("YOOOO its backup time motha fucker!");
+                goto PressAnyKeyToExit;
+            }
+            /*Console.WriteLine("Backing up files...");
+            if (Backup("", SourceDrivePath, DestinationDrivePath))
+            {
+                goto PressAnyKeyToExit;
+            }
+            Console.WriteLine("Pruning removed files...");
+            if (Prune("", SourceDrivePath, DestinationDrivePath))
+            {
+                goto PressAnyKeyToExit;
+            }*/
+            Console.WriteLine("Pushing all git repos...");
+            if (BackupGitRepos("", SourceDrivePath, DestinationDrivePath))
+            {
+                goto PressAnyKeyToExit;
+            }
+            Console.WriteLine();
+        PressAnyKeyToExit:
+            Console.WriteLine("Press any key to exit...");
+            Stopwatch bufferConsumeStopWatch = Stopwatch.StartNew();
+            while (true)
+            {
+                Console.ReadKey(true);
+                if (bufferConsumeStopWatch.ElapsedTicks > 10000000)
+                {
+                    break;
+                }
+            }
+            Process.GetCurrentProcess().Kill();
+        }
+
+        // Approved 1
+        // Locates the source and destination drives by their .backup marker files.
+        public static bool IdentifyDrives()
+        {
+            Console.WriteLine("Identifying source and destination drives...");
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (File.Exists("\\\\?\\" + drive.Name + "source.backup"))
+                {
+                    SourceDrivePath = "\\\\?\\" + drive.Name;
+                    break;
+                }
+            }
+            if (SourceDrivePath is null)
+            {
+                Console.WriteLine("source.backup marker file could not be found.");
                 Console.WriteLine();
-                BackupFolder("D:\\", "F:\\Backup\\Important Data", "");
-                BackupFolder("E:\\", "F:\\Backup\\Media Archive", "");
+                return true;
+            }
+            Console.WriteLine($"Located source drive \"{SourceDrivePath}\".");
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (File.Exists("\\\\?\\" + drive.Name + "destination.backup"))
+                {
+                    DestinationDrivePath = "\\\\?\\" + drive.Name;
+                    break;
+                }
+            }
+            if (DestinationDrivePath is null)
+            {
+                Console.WriteLine("destination.backup marker file could not be found.");
                 Console.WriteLine();
-                Console.WriteLine("Backup complete!");
-                Console.PressAnyKeyToExit();
+                return true;
             }
-            catch (System.Exception ex)
-            {
-                Console.WriteFatalError(ex);
-            }
+            Console.WriteLine($"Located destination drive \"{DestinationDrivePath}\".");
+
+            Console.WriteLine();
+            return false;
         }
-        private static void BackupFolder(string sourcePath, string destinationPath, string subSourcePath)
+
+        // Scans through source drive for git repos and makes sure all changes are commited and pushed to remote origin.
+        public static bool BackupGitRepos(string subPath, string source, string destination)
         {
-            string sourceFolderPath;
-            string destinationFolderPath;
-            if (subSourcePath is null || subSourcePath.Length is 0)
+            // Run git backup commands if this is a git repo.
+            bool gitCommit = false; // Don't run git commands by default.
+            if(subPath.Length == 0 && Directory.Exists(source + ".git"))
             {
-                sourceFolderPath = sourcePath;
-                destinationFolderPath = destinationPath;
+                gitCommit = true; // Run git commands if the folder is a git repo.
             }
-            else
+            else if (subPath.Length != 0 && Directory.Exists(source + subPath + "\\.git"))
             {
-                sourceFolderPath = $"{sourcePath}\\{subSourcePath}";
-                destinationFolderPath = $"{destinationPath}\\{subSourcePath}";
+                gitCommit = true; // Run git commands if the folder is a git repo.
             }
-            try
+            if (gitCommit)
             {
-                if (!System.IO.Directory.Exists(destinationFolderPath))
+                string out1 = RunCommand("git rm --cached -r *", source + subPath);
+                string out2 = RunCommand("git add *", source + subPath);
+                string out3 = RunCommand("git commit -m\"Backup Commit\"", source + subPath);
+                string out4 = RunCommand("git push origin --all", source + subPath);
+            }
+
+            // Recursively check sub folders.
+            foreach (string sourceFolder in Directory.GetDirectories(source + subPath))
+            {
+                bool scan = true; // Scan sub folders by default.
+                if (subPath.Length == 0 && (new DirectoryInfo(sourceFolder).Attributes & (FileAttributes.Hidden | FileAttributes.System)) != 0)
                 {
-                    System.IO.Directory.CreateDirectory(destinationFolderPath);
+                    scan = false; // Don't scan hidden or system folders in the root of source.
                 }
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteError($"Unable to create destination directory at \"{destinationFolderPath}\" due to error \"{ex.Message}\"");
-                return;
-            }
-            try
-            {
-                string[] files = System.IO.Directory.GetFiles(sourceFolderPath);
-                foreach (string file in files)
+                if (scan)
                 {
-                    try
-                    {
-                        BackupFile(file, destinationFolderPath + "\\" + System.IO.Path.GetFileName(file));
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteError($"Unable to backup file \"{file}\" due to error \"{ex.Message}\"");
-                    }
-                }
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteError($"Unable to get files within folder \"{sourceFolderPath}\" due to error \"{ex.Message}\"");
-            }
-            try
-            {
-                string[] folders = System.IO.Directory.GetDirectories(sourceFolderPath);
-                foreach (string folder in folders)
-                {
-                    try
-                    {
-                        BackupFolder(sourcePath, destinationPath, folder.Substring(sourcePath.Length));
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteError($"Unable to backup folder \"{folder}\" due to error \"{ex.Message}\"");
-                    }
-                }
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteError($"Unable to get folders within folder \"{sourceFolderPath}\" due to error \"{ex.Message}\"");
-            }
-        }
-        private static void BackupFile(string sourcePath, string destinationPath)
-        {
-            System.IO.FileStream sourceFileStream;
-            try
-            {
-                sourceFileStream = System.IO.File.Open(sourcePath, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.None);
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteError($"Unable to open file stream for \"{sourcePath}\" due to error \"{ex.Message}\"");
-                return;
-            }
-            System.IO.FileStream destinationFileStream;
-            try
-            {
-                destinationFileStream = System.IO.File.Open(destinationPath, System.IO.FileMode.CreateNew, System.IO.FileAccess.Write, System.IO.FileShare.None);
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteError($"Unable to open file stream for \"{sourcePath}\" due to error \"{ex.Message}\"");
-                return;
-            }
-            try
-            {
-                byte[] buffer = new byte[8192];
-                int bytesRead = sourceFileStream.Read(buffer, 0, buffer.Length);
-                while (bytesRead > 0)
-                {
-                    destinationFileStream.Write(buffer, 0, bytesRead);
-                    bytesRead = sourceFileStream.Read(buffer, 0, buffer.Length);
-                }
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteError($"Unable to copy bytes from \"{sourcePath}\" to \"{destinationPath}\" due to error \"{ex.Message}\"");
-            }
-            finally
-            {
-                try
-                {
-                    sourceFileStream.Close();
-                }
-                catch (System.Exception ex)
-                {
-                    Console.WriteWarning($"Unable to close file stream for \"{sourcePath}\" due to error \"{ex.Message}\"");
-                }
-                try
-                {
-                    destinationFileStream.Close();
-                }
-                catch (System.Exception ex)
-                {
-                    Console.WriteWarning($"Unable to close file stream for \"{destinationPath}\" due to error \"{ex.Message}\"");
+                    BackupGitRepos(sourceFolder.Substring(source.Length), source, destination);
                 }
             }
 
-            BackedUpFileCount++;
-            System.Console.Title = $"Backed up {BackedUpFileCount} files";
+            return false;
+        }
+
+        // Approved 1
+        // Prunes files and folders from destination which have been deleted from source recursively.
+        public static bool Prune(string subPath, string source, string destination)
+        {
+            // Prune folders from backup which have been deleted from source.
+            foreach (string destinationFolder in Directory.GetDirectories(destination + subPath))
+            {
+                string sourceFolder = source + destinationFolder.Substring(destination.Length);
+                bool prune = !Directory.Exists(sourceFolder); // Prune folders which don't exist in source.
+                if (subPath.Length == 0 && (new DirectoryInfo(destinationFolder).Attributes & (FileAttributes.Hidden | FileAttributes.System)) != 0)
+                {
+                    prune = false; // Don't prune hidden or system folders in the root of destination.
+                }
+                if (prune)
+                {
+                    Attempt(() => { Directory.Delete(destinationFolder, true); });
+                }
+            }
+
+            // Prune files from destination which have been deleted from source.
+            foreach (string destinationFile in Directory.GetFiles(destination + subPath))
+            {
+                string sourceFile = source + destinationFile.Substring(destination.Length);
+                bool prune = !File.Exists(sourceFile); // Prune files which don't exist in source.
+                if (destinationFile == destination + "destination.backup")
+                {
+                    prune = false; // Don't prune the destination.backup file.
+                }
+                if (prune)
+                {
+                    Attempt(() => { File.Delete(destinationFile); });
+                }
+            }
+
+            // Recursively calls this method on sub folders of the current folder.
+            foreach (string destinationFolder in Directory.GetDirectories(destination + subPath))
+            {
+                bool prune = true; // Prune sub folders by default.
+                if (subPath.Length == 0 && (new DirectoryInfo(destinationFolder).Attributes & (FileAttributes.Hidden | FileAttributes.System)) != 0)
+                {
+                    prune = false; // Don't prune hidden or system folders in the root of destination.
+                }
+                if (prune)
+                {
+                    Prune(destinationFolder.Substring(source.Length), source, destination);
+                }
+            }
+
+            return false;
+        }
+
+        // Approved 1
+        // Backs up files from source to destination which have been modified recursively.
+        public static bool Backup(string subPath, string source, string destination)
+        {
+            // Create the destination directory if it doesn't exist already.
+            if (!Directory.Exists(destination + subPath))
+            {
+                Attempt(() => { Directory.CreateDirectory(destination + subPath); });
+            }
+
+            // Back up files in source.
+            foreach (string sourceFile in Directory.GetFiles(source + subPath))
+            {
+                string destinationFile = destination + sourceFile.Substring(source.Length);
+                bool backup = false; // Don't backup files by default
+                if (!File.Exists(destinationFile))
+                {
+                    backup = true; // Backup files that do not exist in the destination yet.
+                }
+                if (new FileInfo(sourceFile).LastWriteTime.Ticks - new FileInfo(destinationFile).LastWriteTime.Ticks > 600000000L)
+                {
+                    backup = true; // Backup files modified since last backup or within 1 minute of last backup.
+                }
+                if (sourceFile == source + "source.backup")
+                {
+                    backup = false; // Don't backup source.backup file.
+                }
+                if (backup)
+                {
+                    Attempt(() => { File.Copy(sourceFile, destinationFile, true); });
+                }
+            }
+
+            // Recursively backup sub folders of source.
+            foreach (string sourceFolder in Directory.GetDirectories(source + subPath))
+            {
+                bool backup = true; // Backup sub folders by default.
+                if (subPath.Length == 0 && (new DirectoryInfo(sourceFolder).Attributes & (FileAttributes.Hidden | FileAttributes.System)) != 0)
+                {
+                    backup = false; // Don't backup hidden or system folders in the root of source.
+                }
+                if (backup)
+                {
+                    Backup(sourceFolder.Substring(source.Length), source, destination);
+                }
+            }
+
+            return false;
+        }
+
+        // Approved 1
+        // Runs a cmd command in a certain working directory.
+        public static string RunCommand(string command, string workingDirectory)
+        {
+            if (workingDirectory.StartsWith("\\\\?\\"))
+            {
+                workingDirectory = workingDirectory.Substring("\\\\?\\".Length);
+            }
+            // Create a process start info
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "cmd.exe"; // Use cmd.exe to run the command
+            processStartInfo.Arguments = $"/c {command}"; // Pass the command to be executed
+            processStartInfo.WorkingDirectory = workingDirectory; // Set the working directory
+            processStartInfo.RedirectStandardOutput = true; // Redirect standard output
+            processStartInfo.RedirectStandardError = true; // Redirect standard error
+            processStartInfo.UseShellExecute = false; // Don't use the shell to execute the command
+//            processStartInfo.CreateNoWindow = true; // Don't create a window
+
+            // Run process and get output.
+            Process process = Process.Start(processStartInfo);
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(process.StandardError.ReadToEnd());
+            Console.ForegroundColor = ConsoleColor.White;
+            process.WaitForExit();
+
+            return "";
+        }
+
+        public static void Attempt(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
     }
 }
